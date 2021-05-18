8782e221c2eba698f236c8a7ceb447d4289d36f3
fadi001 <fzaki001@ucr.edu>
2021-05-18 13:51:23 -0700

    Added function for copy cinstructor of Task class

--- source/task.cpp
+++ source/task.cpp
@@ -1,4 +1,11 @@
 #include "../header/listelements.hpp"
 #include "../header/task.hpp"
 
-Task:://start adding functions
+Task::Task(const Task &t){
+
+	priority = t.priority;
+	dueDate = t.dueDate;
+	taskDesc = t.taskDesc;
+	isDone = false;
+
+}
