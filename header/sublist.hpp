9742263d91fbc423e15a5869bfbc1fda395520b5
rishabdudhia <rishabdudhia@gmail.com>
2021-05-18 12:01:25 -0700

    Added sublist names

--- header/sublist.hpp
+++ header/sublist.hpp
@@ -7,15 +7,18 @@
 class Sublist : public ListElements {
     private:
 	int size;
+	string listName;
 	vector<ListElements*> theList;
 	void sortList();
 
     public:
-	Sublist() {
+	Sublist(string name) : listName(name) {
 		priority = 11;
 		isDone = false;
 		size = 0;
 	}
+	void setName (const string name);
+	string getName () const;
 	Sublist (const Sublist &sub);
 	~Sublist();
 	void add (ListElements* input);
