5b8df93e8b43a6ecd731abc5b3fdf60148ff2486
fadi001 <fzaki001@ucr.edu>
2021-05-18 02:17:13 -0700

    Added copy constructer for sublist

--- source/sublist.cpp
+++ source/sublist.cpp
@@ -2,3 +2,11 @@
 #include "../header/sublist.hpp"
 
 Sublist:://start adding funcs
+
+Subblist::Sublist (const Sublist &sub){
+
+	theList = sub.theList;
+	priority = sub.priority;
+	isDone = false;
+
+}
