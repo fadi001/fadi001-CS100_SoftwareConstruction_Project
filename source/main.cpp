ee02aa8bf8dfe31fd196e04d7f71117554e6aa09
rishabdudhia <rishabdudhia@gmail.com>
2021-05-28 11:39:49 -0700

    Working on main

--- source/main.cpp
+++ source/main.cpp
@@ -11,22 +11,24 @@ char main_menu(Sublist* currList);
 int main() {
 	Sublist* curr = nullptr;
 	char entered = main_menu(curr);
-	string inputName;
+	string inputNameS;
+	string inputNameT;
 	string inputDate;
 	string inputDesc;
 	int p;
+	cin.clear();
 		
 	while (entered != 'q') {
 		if (entered == 'a') {
 			cout << "Please enter a name for your new list/sublist: ";
-			getline(cin, inputName);
-			Sublist* list = new Sublist(inputName);
+			getline(cin, inputNameS);
+			Sublist* list = new Sublist(inputNameS);
 			curr = list;
 		}
 		else if (entered == 'b') {
 			cout << "Enter following task information: " << endl;
 			cout << "Task name: ";
-			getline(cin, inputName);
+			getline(cin, inputNameT);
 			cin.clear();
 
 			cout << "Enter task due date (ex. 12/5/2021): " << endl;
@@ -41,7 +43,7 @@ int main() {
 			getline(cin, inputDesc);
 			cin.clear();
 			
-			Task* task = new Task(p, inputDate, inputName, inputDesc);
+			Task* task = new Task(p, inputDate, inputNameT, inputDesc);
 			curr->add(task);
 		}
 		else if (entered == 'c') {
@@ -60,7 +62,7 @@ char main_menu(Sublist* currList) {
 		cout << "No list created" << endl;
 	}
 	else {
-		cout << currList->getName();
+		cout << currList->getName() << endl;
 	}
 	cout << "Please select an action: " << endl << endl;
 	cout << "(a) Create a sublist (Choose this option to create initial list)" << endl;
