05f239e879208d6e41a1bbdb173019f26da7540d
fadi001 <fzaki001@ucr.edu>
2021-05-31 14:31:20 -0700

    testing file added

---
+++ tests/sublist_task_copy_test.hpp
@@ -0,0 +1,22 @@
+#ifndef __SUBLIST_TASK_COPY_TEST_HPP__
+#define __SUBLIST_TASK_COPY_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "../header/sublist.hpp"
+#include "../header/task.hpp"
+
+TEST(CopySublistTest, CopySubList1){
+	ListElements* curr = new Sublist("Initial List");
+	Task* task = new Task(3, "11/17/2001", "Birth", "The day I was born");
+			curr->add(task);
+			task->setParent(curr);
+
+	Sublist* newOne = curr;
+	
+	EXPECT_EQ(newOne, curr);
+	delete curr;
+	delete newOne;
+}
+
+#endif
